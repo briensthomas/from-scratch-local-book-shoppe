574962e8468c1eb655a06f254e6eeffd60d156d6
Brien Thomas <thomasbrien94@gmail.com>
2022-08-01 16:39:16 -0700

    tests written for the /books route, so you can't fail me Julius

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,21 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
-  });
+  it('#GET /books should return a list of books with their title and released date',
+    async () => {
+      const res = await request(app).get('/books');
+      expect(res.status).toBe(200);
+      expect(res.body).toEqual({
+        title: expect.any(String),
+        year: expect.any(String),
+      });
+    });
   afterAll(() => {
     pool.end();
   });
