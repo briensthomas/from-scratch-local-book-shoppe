5b5f5896e3b2b8820df0c7a2e2247e7c1563814f
Brien Thomas <thomasbrien94@gmail.com>
2022-08-01 17:42:35 -0700

    test for Book model passes: changed test slightly, added additional parameters for robust confirmation

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,16 +7,19 @@ describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('#GET /books should return a list of books with their title and released date',
-    async () => {
-      const res = await request(app).get('/books');
-      expect(res.status).toBe(200);
-      expect(res.body).toEqual({
-        title: expect.any(String),
-        year: expect.any(String),
-      });
+  it('#GET /books should return a list of books id, title, and released date', async () => {
+    const res = await request(app).get('/books');
+    expect(res.status).toBe(200);
+    expect(res.body.length).toBe(5);
+    expect(res.body[0]).toEqual({
+      id: '1',
+      title: 'Dreadgod',
+      released: 2022,
     });
+  });
   afterAll(() => {
     pool.end();
   });
 });
+
+
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,20 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE books (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title VARCHAR,
+    released INT
+);
+
+INSERT INTO books (
+    title,
+    released
+)
+VALUES 
+('Dreadgod', 2022),
+('The Hunger of the Gods', 2022),
+('The Pariah', 2021),
+('Bastion', 2021),
+('Assassin''s Apprentice', 1996)
